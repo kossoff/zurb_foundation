b582324a12b82845f06b8a9d59287b20f9afcb9d
Chris Lee <iam@chrisjlee.net>
2013-03-31 10:51:18 -0500

    update html head

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -42,10 +42,10 @@
  */
 ?>
 <!DOCTYPE html>
-<!-- Sorry no IE 7 support! -->
+<!-- Sorry no IE7 support! -->
 <!-- @see http://foundation.zurb.com/docs/index.html#basicHTMLMarkup -->
 
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 8]><html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 
 <head>
