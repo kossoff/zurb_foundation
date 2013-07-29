52a9c301de90e0e35d5bea000332ce2ec8e3f37c
Alex Weber <alexweber15@gmail.com>
2013-07-29 18:16:43 -0300

    Issue #2052993 by alexweber: Refactor settings forms.

--- theme-settings.php
+++ theme-settings.php
@@ -14,8 +14,8 @@ function zurb_foundation_form_system_theme_settings_alter(&$form, &$form_state) 
       '#weight' => -10,
     );
 
-    /**
-     * General settings.
+    /*
+     * General Settings.
      */
     $form['zurb_foundation']['general'] = array(
       '#type' => 'fieldset',
@@ -31,22 +31,16 @@ function zurb_foundation_form_system_theme_settings_alter(&$form, &$form_state) 
     $form['zurb_foundation']['general']['favicon'] = $form['favicon'];
     unset($form['favicon']);
 
-    /**
-     * Foundation settings.
+    /*
+     * Foundation Top Bar.
      */
-    $form['zurb_foundation']['foundation'] = array(
+    $form['zurb_foundation']['topbar'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Foundation Settings'),
-    );
-
-    $form['zurb_foundation']['foundation']['top_bar'] = array(
-      '#type' => 'fieldset',
-      '#title' => t('Top Bar'),
+      '#title' => t('Foundation Top Bar'),
       '#description' => t('The Foundation Top Bar gives you a great way to display a complex navigation bar on small or large screens.'),
-      '#collapsible' => TRUE,
     );
 
-    $form['zurb_foundation']['foundation']['top_bar']['zurb_foundation_top_bar_enable'] = array(
+    $form['zurb_foundation']['topbar']['zurb_foundation_top_bar_enable'] = array(
       '#type' => 'select',
       '#title' => t('Enable'),
       '#description' => t('If enabled, the site name and main menu will appear in a bar along the top of the page.'),
@@ -58,7 +52,8 @@ function zurb_foundation_form_system_theme_settings_alter(&$form, &$form_state) 
       '#default_value' => theme_get_setting('zurb_foundation_top_bar_enable'),
     );
 
-    $form['zurb_foundation']['foundation']['top_bar']['zurb_foundation_top_bar_grid'] = array(
+
+    $form['zurb_foundation']['topbar']['zurb_foundation_top_bar_grid'] = array(
       '#type' => 'checkbox',
       '#title' => t('Contain to grid'),
       '#description' => t('Check this for your top bar to be set to your grid width.'),
@@ -70,7 +65,7 @@ function zurb_foundation_form_system_theme_settings_alter(&$form, &$form_state) 
       ),
     );
 
-    $form['zurb_foundation']['foundation']['top_bar']['zurb_foundation_top_bar_sticky'] = array(
+    $form['zurb_foundation']['topbar']['zurb_foundation_top_bar_sticky'] = array(
       '#type' => 'checkbox',
       '#title' => t('Sticky'),
       '#description' => t('Check this for your top bar to stick to the top of the screen when the user scrolls down. If you\'re using the Admin Menu module and have it set to \'Keep menu at top of page\', you\'ll need to check this option to maintain compatibility.'),
@@ -82,7 +77,7 @@ function zurb_foundation_form_system_theme_settings_alter(&$form, &$form_state) 
       ),
     );
 
-    $form['zurb_foundation']['foundation']['top_bar']['zurb_foundation_top_bar_menu_text'] = array(
+    $form['zurb_foundation']['topbar']['zurb_foundation_top_bar_menu_text'] = array(
       '#type' => 'textfield',
       '#title' => t('Menu text'),
       '#description' => t('Specify text to go beside the mobile menu icon or leave blank for none.'),
@@ -94,13 +89,16 @@ function zurb_foundation_form_system_theme_settings_alter(&$form, &$form_state) 
       ),
     );
 
-    $form['zurb_foundation']['foundation']['messages'] = array(
+    /*
+     * Status Messages.
+     */
+    $form['zurb_foundation']['messages'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Messages'),
+      '#title' => t('Status Messages'),
       '#collapsible' => TRUE,
     );
 
-    $form['zurb_foundation']['foundation']['messages']['modal'] = array(
+    $form['zurb_foundation']['messages']['modal'] = array(
       '#type' => 'checkbox',
       '#title' => t('Display status messages in a modal'),
       '#description' => t('Check this to display Drupal status messages in a Foundation reveal modal.'),
