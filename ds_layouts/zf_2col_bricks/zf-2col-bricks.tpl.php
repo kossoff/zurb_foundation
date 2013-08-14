ebd651123e02a48dd260b4db34da89760980d6e8
Alex Weber <alexweber15@gmail.com>
2013-08-14 10:43:42 -0600

    fix typo in 2 col bricks layout wrapper classes

--- ds_layouts/zf_2col_bricks/zf-2col-bricks.tpl.php
+++ ds_layouts/zf_2col_bricks/zf-2col-bricks.tpl.php
@@ -4,7 +4,7 @@
  * Template for Zurb Foundation Two column bricks Display Suite layout.
  */
 ?>
-<<?php print $layout_wrapper; print $layout_attributes; ?> class="row zf-2col-stacked <?php print $classes;?> clearfix">
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="row zf-2col-bricks <?php print $classes;?> clearfix">
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
     <?php print render($title_suffix['contextual_links']); ?>
