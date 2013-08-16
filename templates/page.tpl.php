aca9b2d82dba51b7a35c3e675fcd7dc17940e6fa
Alex Weber <alexweber15@gmail.com>
2013-08-15 23:37:22 -0600

    Issue #2063817 by alexweber: Added Update base theme's page.tpl.php.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,92 +1,108 @@
-<!-- Top bar -->
-<?php if ($top_bar): ?>
-  <?php if ($top_bar_classes): ?>
-    <div class="<?php print $top_bar_classes; ?>">
-  <?php endif; ?>
-    <nav class="top-bar"<?php print $top_bar_options; ?>>
-      <ul class="title-area">
-        <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
-        <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
-      </ul>
-      <section class="top-bar-section">
-        <?php if ($top_bar_main_menu) :?>
-          <?php print $top_bar_main_menu; ?>
+<!--.page -->
+<div role="document" class="page">
+
+  <!--.l-header region -->
+  <header role="banner" class="l-header">
+
+    <?php if ($top_bar): ?>
+      <!--.top-bar -->
+      <?php if ($top_bar_classes): ?>
+        <div class="<?php print $top_bar_classes; ?>">
+      <?php endif; ?>
+      <nav class="top-bar"<?php print $top_bar_options; ?>>
+        <ul class="title-area">
+          <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
+          <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
+        </ul>
+        <section class="top-bar-section">
+          <?php if ($top_bar_main_menu) :?>
+            <?php print $top_bar_main_menu; ?>
+          <?php endif; ?>
+          <?php if ($top_bar_secondary_menu) :?>
+            <?php print $top_bar_secondary_menu; ?>
+          <?php endif; ?>
+        </section>
+      </nav>
+      <?php if ($top_bar_classes): ?>
+        </div>
+      <?php endif; ?>
+      <!--/.top-bar -->
+    <?php endif; ?>
+
+    <!-- Title, slogan and menu -->
+    <?php if ($alt_header): ?>
+      <section class="row <?php print $alt_header_classes; ?>">
+
+        <?php if ($site_slogan): ?>
+          <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-        <?php if ($top_bar_secondary_menu) :?>
-          <?php print $top_bar_secondary_menu; ?>
+
+        <?php if ($alt_main_menu): ?>
+          <nav id="main-menu" class="navigation" role="navigation">
+            <?php print ($alt_main_menu); ?>
+          </nav> <!-- /#main-menu -->
         <?php endif; ?>
+
+        <?php if ($alt_secondary_menu): ?>
+          <nav id="secondary-menu" class="navigation" role="navigation">
+            <?php print $alt_secondary_menu; ?>
+          </nav> <!-- /#secondary-menu -->
+        <?php endif; ?>
+
       </section>
-    </nav>
-  <?php if ($top_bar_classes): ?>
-    </div>
-  <?php endif; ?>
-<?php endif; ?>
-<!-- End top bar -->
-
-<!-- Title, slogan and menu -->
-<?php if ($alt_header): ?>
-  <div class="row <?php print $alt_header_classes; ?>">
-
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
     <?php endif; ?>
+    <!-- End title, slogan and menu -->
 
-    <?php if ($site_name): ?>
-      <h1 title="<?php print $site_name; ?>" id="site-name" class="site-name">
-        <a href="<?php print $front_page; ?>" title="<?php print $site_name; ?>"><?php print $site_name; ?></a>
-      </h1>
-    <?php endif; ?>
+  </header>
+  <!--/.l-header -->
 
-    <?php if ($site_slogan): ?>
-      <h2 title="<?php print $site_slogan; ?>" id="site-slogan" class="site-slogan"><?php print $site_slogan; ?></h2>
-    <?php endif; ?>
-
-    <?php if ($alt_main_menu): ?>
-      <nav id="main-menu" class="navigation" role="navigation">
-        <?php print ($alt_main_menu); ?>
-      </nav> <!-- /#main-menu -->
-    <?php endif; ?>
-
-    <?php if ($alt_secondary_menu): ?>
-      <nav id="secondary-menu" class="navigation" role="navigation">
-        <?php print $alt_secondary_menu; ?>
-      </nav> <!-- /#secondary-menu -->
-    <?php endif; ?>
-  </div>
-<?php endif; ?>
-<!-- End title, slogan and menu -->
-
-<?php if (($messages && !$zurb_foundation_messages_modal) || !empty($page['help'])): ?>
-<div class="row">
-  <div id="messages" class="large-12 columns">
-    <?php if ($messages): print $messages; endif; ?>
-    <?php if (!empty($page['help'])): print render($page['help']); endif; ?>
-  </div>
-</div>
-<?php endif; ?>
-
-<div class="row">
-  <!--#main -->
-  <div id="main" class="<?php print $main_grid; ?> columns">
-
-
-    <?php if (!empty($page['highlighted'])): ?>
-      <div class="highlight panel callout">
-        <?php print render($page['highlighted']); ?>
+  <?php if (!empty($page['featured'])): ?>
+    <!--/.featured -->
+    <section class="l-featured row">
+      <div class="large-12 columns">
+        <?php print render($page['featured']); ?>
       </div>
-    <?php endif; ?>
+    </section>
+    <!--/.l-featured -->
+  <?php endif; ?>
 
-    <a id="main-content"></a>
+  <?php if ($messages && !$zurb_foundation_messages_modal): ?>
+    <!--/.l-messages -->
+    <section class="l-messages row">
+      <div class="large-12 columns">
+        <?php if ($messages): print $messages; endif; ?>
+      </div>
+    </section>
+    <!--/.l-messages -->
+  <?php endif; ?>
 
-    <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+  <?php if (!empty($page['help'])): ?>
+    <!--/.l-help -->
+    <section class="l-help row">
+      <div class="large-12 columns">
+        <?php print render($page['help']); ?>
+      </div>
+    </section>
+    <!--/.l-help -->
+  <?php endif; ?>
 
-    <?php if ($title && !$is_front): ?>
-      <?php print render($title_prefix); ?>
-      <h1 id="page-title" class="title"><?php print $title; ?></h1>
-      <?php print render($title_suffix); ?>
-    <?php endif; ?>
+  <main role="main" class="row l-main">
+    <div class="<?php print $main_grid; ?> main columns">
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlight panel callout">
+          <?php print render($page['highlighted']); ?>
+        </div>
+      <?php endif; ?>
+
+      <a id="main-content"></a>
+
+      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
+
+      <?php if ($title && !$is_front): ?>
+        <?php print render($title_prefix); ?>
+        <h1 id="page-title" class="title"><?php print $title; ?></h1>
+        <?php print render($title_suffix); ?>
+      <?php endif; ?>
 
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
@@ -99,61 +115,52 @@
         </ul>
       <?php endif; ?>
 
-    <?php print render($page['content']); ?>
+      <?php print render($page['content']); ?>
+    </div>
+    <!--/.main region -->
 
-
-  </div><!--/#main -->
-
-
-  <?php if (!empty($page['sidebar_first'])): ?>
-  <!--#sidebar-first -->
-    <div id="sidebar-first" class="<?php print $sidebar_first_grid; ?> columns sidebar ">
-      <?php print render($page['sidebar_first']); ?>
-    </div><!--/#sidebar-first-->
-  <?php endif; ?>
-
-  <?php if (!empty($page['sidebar_second'])): ?>
-  <!--#sidebar-second -->
-    <div id="sidebar-second" class="<?php print $sidebar_sec_grid;?> columns sidebar">
-      <?php print render($page['sidebar_second']); ?>
-    </div><!--/#sidebar-second -->
-  <?php endif; ?>
-
-</div> <!--/.row -->
-
-<?php if (!empty($page['footer_first']) || !empty($page['footer_middle']) || !empty($page['footer_last'])): ?>
-<footer class="row">
-    <?php if (!empty($page['footer_first'])): ?>
-      <div id="footer-first" class="large-4 columns">
-        <?php print render($page['footer_first']); ?>
-      </div>
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_first_grid; ?> sidebar-first columns sidebar">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>
     <?php endif; ?>
-    <?php if (!empty($page['footer_middle'])): ?>
-      <div id="footer-middle" class="large-4 columns">
-        <?php print render($page['footer_middle']); ?>
-      </div>
-    <?php endif; ?>
-    <?php if (!empty($page['footer_last'])): ?>
-      <div id="footer-last" class="large-4 columns">
-        <?php print render($page['footer_last']); ?>
-      </div>
-    <?php endif; ?>
-</footer>
-<?php endif; ?>
 
-<div class="bottom-bar panel">
-  <div class="row">
-    <div class="large-6 columns">
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_sec_grid; ?> sidebar-second columns sidebar">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>
+    <?php endif; ?>
+  </main>
+  <!--/.main-->
+
+  <?php if (!empty($page['footer_first']) || !empty($page['footer_middle']) || !empty($page['footer_last'])): ?>
+    <!--.l-footer-->
+    <footer class="l-footer panel row" role="contentinfo">
+      <?php if (!empty($page['footer_first'])): ?>
+        <div id="footer-first" class="large-4 columns">
+          <?php print render($page['footer_first']); ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_middle'])): ?>
+        <div id="footer-middle" class="large-4 columns">
+          <?php print render($page['footer_middle']); ?>
+        </div>
+      <?php endif; ?>
+      <?php if (!empty($page['footer_last'])): ?>
+        <div id="footer-last" class="large-4 columns">
+          <?php print render($page['footer_last']); ?>
+        </div>
+      <?php endif; ?>
+
       <?php if ($site_name) :?>
-        &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
+        <div class="copyright large-12 columns">
+          &copy; <?php print date('Y') . ' ' . check_plain($site_name) . ' ' . t('All rights reserved.'); ?>
+        </div>
       <?php endif; ?>
-    </div>
-    <div class="large-6 small-12 columns">
-      <?php if(!empty($page['bottom_menu'])) :?>
-        <?php print render($page['bottom_menu']); ?>
-      <?php endif; ?>
-    </div>
-  </div>
-</div>
+    </footer>
+    <!--/.footer-->
+  <?php endif; ?>
 
-<?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
+  <?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
+</div>
+<!--/.page -->
